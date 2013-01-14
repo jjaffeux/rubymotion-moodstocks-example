27da22846a9a3b1157a105f7da125c24fd3aac23
Joffrey Jaffeux <j.jaffeux@gmail.com>
2013-01-14 11:55:21 +0100

    typo

--- app/spinner.rb
+++ app/spinner.rb
@@ -2,7 +2,7 @@ module Spinner
 
   def startSpinner
     @spinner = UIActivityIndicatorView.alloc.initWithActivityIndicatorStyle(UIActivityIndicatorViewStyleGray)
-    @spinner.center = [self.view.size.width / 2, self.view.size.height / 2,]
+    @spinner.center = [self.view.size.width / 2, self.view.size.height / 2]
     @spinner.hidesWhenStopped = true
     @spinner.startAnimating
     self.view.addSubview(@spinner)
